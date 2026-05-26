bd244a8d5b6c96bf81291f0854de6a878b327bdd
Gabriela Hernandez <gabrielasuarez120104@gmail.com>
2026-05-26 17:52:08 -0500

    "Actualiza modelo de pregunta para la trivia"

--- -kotlin-todo-cli--main/Trivia_preguntas/Preguntas.kt
+++ -kotlin-todo-cli--main/Trivia_preguntas/Preguntas.kt
@@ -1,9 +1,8 @@
 package trivia_preguntas
 
-// Data class requerida por la rúbrica para modelar una pregunta de la trivia
 data class Pregunta(
     val id: Int,
     val enunciado: String,
     val opciones: List<String>,
-    val respuestaCorrecta: Int // Índice de la opción correcta (1 a 4)
+    val respuestaCorrecta: Int
 )
